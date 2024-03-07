6b1362eb8feb6018118cb5179bf9f6c401b8d882
hibakenafi <kenafi.hiba@etu.uae.ac.ma>
2024-03-07 23:30:55 +0100

    update

---
+++ Professional_Networking_Platform/PNP/models/message.py
@@ -0,0 +1,39 @@
+
+import json
+from django.db import models
+
+class Messages(models.Model):
+    messages = models.TextField(default="[]")
+    media = models.TextField(default="[]")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    sender = models.ForeignKey('PNP.User', on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey('PNP.User', on_delete=models.CASCADE, related_name='received_messages')
+
+    def send_message(self, new_message):
+        current_messages = self.get_messages_list()
+        current_messages.append(new_message)
+        self.messages = json.dumps(current_messages)
+        self.save()
+
+    def delete_message(self, index):
+        current_messages = self.get_messages_list()
+        if 0 <= index < len(current_messages):
+            del current_messages[index]
+            self.messages = json.dumps(current_messages)
+            self.save()
+
+    def receive_message(self):
+        return self.get_messages_list()
+
+    def get_messages_list(self):
+        try:
+            return json.loads(self.messages)
+        except (json.JSONDecodeError, ValueError):
+            return []
+
+    def get_media_list(self):
+        try:
+            return json.loads(self.media)
+        except (json.JSONDecodeError, ValueError):
+            return []
---
+++ Professional_Networking_Platform/PNP/models/share.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class share(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('PNP.User', on_delete=models.DO_NOTHING)
+    post_id = models.ForeignKey('PNP.Post', on_delete=models.DO_NOTHING)
+    #
+    comment = models.TextField()
+    #
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
